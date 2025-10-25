a8a76b71726357ca92ba6635c9759790ac6c0f5a
unknown <s293527@studenti.polito.it>
2025-10-25 12:51:13 +0200

    cose

--- Factorizations/PLU_Decomposition.py
+++ Factorizations/PLU_Decomposition.py
@@ -1,15 +1,19 @@
 import numpy as np
 
-def plu_decomposition(A, tol = 1e-8):
+def PLU_Decomposition(A, tol = 1e-8):
 
 
     """
-    This function performs the PA = LU decomposition. Where P is a permutation matrix, L is lower triag and U is upper triag.
+    This function performs the PA = LU decomposition for rectangular matrices. Where P is a permutation matrix, L is lower triag and U is upper triag.
     tol is the tollerance 
     """
 
     A = A.copy()
     m,n = A.shape
+
+    if m != n:
+        raise ValueError("Number of rows must be greater than or equal to number of columns")
+    
     P = np.eye(m)
     L = np.zeros((m,n))
     U = A.copy()
@@ -31,20 +35,28 @@ def plu_decomposition(A, tol = 1e-8):
             U[i,k:] -= L[i,k] * U[k,k:]
     
     np.fill_diagonal(L,1)
+    norm_result = np.linalg.norm(P @ A - L @ U)
 
-    return P,L,U
+    return P,L,U, norm_result
 
 def main_test():
 
     A = np.array([[0,2,1],[1,1,0],[2,1,1]],dtype=float)
-    P,L,U = plu_decomposition(A)
+    P,L,U,norm = PLU_Decomposition(A)
     print("A =\n",A)
     print("P =\n",P)
     print("L =\n",L)
     print("U =\n",U)
-    print("PA =\n",np.dot(P,A))
-    print("LU =\n",np.dot(L,U))
-    print("Difference ||PA - LU|| =\n",np.linalg.norm(P@A - L@U))
+    print("Difference ||PA - LU|| =\n",norm)
+
+    A = np.array([[0,2,1],[1,1,0],[2,1,1],[0,1,4]],dtype=float)
+    P,L,U,norm = PLU_Decomposition(A)
+    print("A =\n",A)
+    print("P =\n",P)
+    print("L =\n",L)
+    print("U =\n",U)
+    print("Difference ||PA - LU|| =\n",norm)
+
 
 if __name__ == "__main__":
     main_test()
